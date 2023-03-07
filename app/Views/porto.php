3bfa00169b807fdb3bb442a91e33e5978a3c5ccf
Billiardo <Helloobilly@gmail.com>
2023-03-07 09:18:28 +0700

    Beta

---
+++ app/Views/porto.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Minimalist Portfolio</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<header>
+		<h1>My Portfolio</h1>
+	</header>
+
+	<main>
+		<section>
+			<h2>About Me</h2>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce consectetur ut eros id dapibus. Nunc ut tellus nibh. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Ut semper euismod eros vel blandit. Vestibulum lobortis, ex non ultricies tristique, elit elit lacinia dolor, vel mattis sapien ex non nisi. Suspendisse potenti. In quis arcu in velit maximus interdum sit amet et nulla. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed ultrices, nisl vel ullamcorper posuere, velit velit maximus metus, vel hendrerit purus mauris in lacus.</p>
+		</section>
+
+		<section>
+			<h2>My Work</h2>
+			<ul>
+				<li>
+					<a href="#">
+						<img src="https://picsum.photos/200/200" alt="Project 1">
+						<h3>Project 1</h3>
+					</a>
+				</li>
+				<li>
+					<a href="#">
+						<img src="https://picsum.photos/200/200" alt="Project 2">
+						<h3>Project 2</h3>
+					</a>
+				</li>
+				<li>
+					<a href="#">
+						<img src="https://picsum.photos/200/200" alt="Project 3">
+						<h3>Project 3</h3>
+					</a>
+				</li>
+			</ul>
+		</section>
+	</main>
+
+	<footer>
+		<p>&copy; 2023 My Portfolio. All rights reserved.</p>
+	</footer>
+</body>
+</html>
